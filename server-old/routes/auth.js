7db9e136d763b9d8f54ec6cc46d16dd918ad7f76
Mihir Karandikar <mihirkkarandikar@gmail.com>
2022-09-30 13:38:01 +0530

    remove old server

--- server-old/routes/auth.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-const router = express.Router()
-
-import {googlelogin} from '../controllers/auth.js'
-
-router.route('/googlelogin').post(googlelogin);
-
-export default router;
